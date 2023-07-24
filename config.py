1ee402900c2bf67775265e225ebfd1a423a2efb9
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-07-24 11:02:53 +0200

    creation setup.cfget conig.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Config file, secret key session.
+
+# Secret key session management.
+SECRET_KEY = "du^t=6^nu&+e+jqw%!4czaf2ccpwv2!@*9tu566n&=cmvv)fcg"
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,11 @@
 import pytest
+from app.server import showSummary
+
+# verifie si type email non ok => retrun msg error
+# verifie si email dans db => retrun ok
+# verifie si email absent db => return msg error
 
 
 def test_email_is_valid_type():
     # check if email is a valid email.
-
     pass
