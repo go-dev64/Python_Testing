5c967eff864276be3b35d3228b91ab6fc3f2dde0
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-07-31 16:03:22 +0200

    create test_data_returned

--- tests/test_unit_tests/test_server.py
+++ tests/test_unit_tests/test_server.py
@@ -18,10 +18,6 @@ class TestEmail(MockReponse):
         assert rv.status_code == 200
         # Check template returned.
         assert template.name == "welcome.html"
-        # Check whether the context returned is that of the e-mail supplied.
-        assert context["club"]["email"] == self.email
-        # print("rv", rv.data.decode())
-        # print("templates:", template, context)
 
     def test_email_is_not_in_db(self, client, monkeypatch, captured_templates):
         # Mock clubs data json .
@@ -34,9 +30,11 @@ class TestEmail(MockReponse):
         assert b"error" in rv.data
         assert template.name == "index.html"
 
-    """ def test_data_returned(self, client, monkeypatch, captured_templates):
+    def test_data_returned(self, client, monkeypatch, captured_templates):
         # Check whether the context returned is that of the e-mail supplied.
         monkeypatch.setattr(server, "clubs", self.get_clubs())
         rv = client.post("/showSummary", data={"email": self.email})
         template, context = captured_templates[0]
-        assert context["club"]["email"] == self.email"""
+        assert context["club"]["email"] == self.email
+        assert context["list_of_club"] == server.clubs
+        assert len(context["list_of_club"]) > 0
