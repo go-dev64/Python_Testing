c7edfc1dee82d17beb673191c628259e26708c96
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-08-04 12:10:13 +0200

    add test_server/test_several_orders_with_more_than_twelves_places_in_total

--- tests/test_unit_tests/test_server.py
+++ tests/test_unit_tests/test_server.py
@@ -172,6 +172,31 @@ class TestBooking(Utils):
         assert rv.status_code == 400
         assert str(context["error"]) == "The maximum reservation is 12 places!"
 
+    def test_several_orders_with_more_than_twelves_places_in_total(self, client, monkeypatch, captured_templates):
+        """
+        Should return a status_code 400 with places > 12 and error message.
+        """
+        data_test = {"club": "toto", "competition": "Spring Festival", "places": 7}
+        rv, template, context = self.get_response_value_and_template_context(
+            captured_templates=captured_templates,
+            client=client,
+            method="POST",
+            monkeypatch=monkeypatch,
+            route="/purchasePlaces",
+            data=data_test,
+        )
+        assert rv.status_code == 200
+        data_test = {"club": "toto", "competition": "Spring Festival", "places": 7}
+        rv, template, context = self.get_response_value_and_template_context(
+            captured_templates=captured_templates,
+            client=client,
+            method="POST",
+            monkeypatch=monkeypatch,
+            route="/purchasePlaces",
+            data=data_test,
+        )
+        assert rv.status_code == 400
+
     def test_booking_with_purchase_more_than_club_points(self, client, monkeypatch, captured_templates):
         """
         Test should retrun status code 400 with places purchase 7 places) > toto club's points (5 points).
